19b3ebfa0a2e5a3580c57060a6065c1654ec34eb
Vladimir Jovanovic <vladimir.jovanovic@bookaboat.co>
2014-12-25 18:27:30 +0100

    kostur gotov, krs krs kod

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,11 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+  $('#ajde').click(function() {
+    $('#ajmo').slideToggle('slow');
+  });
+});
+
+$(document).ready(function() {
+  $('#ajdee').click(function() {
+    $('#ajmoo').slideToggle('slow');
+  });
+});
