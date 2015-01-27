78912fca42f578cdfc30dc95ddb66298d47c4a4e
Vladimir Jovanovic <vladimir.jovanovic@bookaboat.co>
2015-01-27 20:00:01 +0100

    final

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,27 +4,15 @@ $(document).ready(function() {
   $('#title1').click(function() {
     $('#list1').slideToggle('fast');
   });
-});
-
-$(document).ready(function() {
   $('#title2').click(function() {
     $('#list2').slideToggle('fast');
   });
-});
-
-$(document).ready(function() {
   $('#title3').click(function() {
     $('#list3').slideToggle('fast');
   });
-});
-
-$(document).ready(function() {
   $('#title4').click(function() {
     $('#list4').slideToggle('fast');
   });
-});
-
-$(document).ready(function() {
   $("#myCarousel").carousel();
 });
 
